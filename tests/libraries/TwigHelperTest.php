1ba4a5e040d0dcc35130964a66b0544db79cb487
kenjis <kenji.uui@gmail.com>
2016-10-08 21:52:12 +0900

    Reformat Test files

--- tests/libraries/TwigHelperTest.php
+++ tests/libraries/TwigHelperTest.php
@@ -1,80 +1,80 @@
 <?php
 
-require __DIR__.'/../../libraries/Twig.php';
+require __DIR__ . '/../../libraries/Twig.php';
 
 class TwigHelperTest extends PHPUnit_Framework_TestCase
 {
-    public static function setUpBeforeClass()
-    {
-        parent::setUpBeforeClass();
-        $CI =& get_instance();
-        $CI->load->helper('url');
-    }
+	public static function setUpBeforeClass()
+	{
+		parent::setUpBeforeClass();
+		$CI =& get_instance();
+		$CI->load->helper('url');
+	}
 
-    public function setUp()
-    {
-        $twig = new Twig();
+	public function setUp()
+	{
+		$twig = new Twig();
 
-        $loader = new Twig_Loader_Array([
-            'base_url' => '{{ base_url(\'"><s>abc</s><a name="test\') }}',
-            'site_url' => '{{ site_url(\'"><s>abc</s><a name="test\') }}',
-            'anchor'   => '{{ anchor(uri, title, attributes) }}',
-        ]);
-        $setLoader = ReflectionHelper::getPrivateMethodInvoker(
-            $twig, 'setLoader'
-        );
-        $setLoader($loader);
+		$loader = new Twig_Loader_Array([
+			'base_url' => '{{ base_url(\'"><s>abc</s><a name="test\') }}',
+			'site_url' => '{{ site_url(\'"><s>abc</s><a name="test\') }}',
+			'anchor' => '{{ anchor(uri, title, attributes) }}',
+		]);
+		$setLoader = ReflectionHelper::getPrivateMethodInvoker(
+			$twig, 'setLoader'
+		);
+		$setLoader($loader);
 
-        $resetTwig = ReflectionHelper::getPrivateMethodInvoker(
-            $twig, 'resetTwig'
-        );
-        $resetTwig();
+		$resetTwig = ReflectionHelper::getPrivateMethodInvoker(
+			$twig, 'resetTwig'
+		);
+		$resetTwig();
 
-        $addFunctions = ReflectionHelper::getPrivateMethodInvoker(
-            $twig, 'addFunctions'
-        );
-        $addFunctions();
+		$addFunctions = ReflectionHelper::getPrivateMethodInvoker(
+			$twig, 'addFunctions'
+		);
+		$addFunctions();
 
-        $this->obj = $twig;
-        $this->twig = $twig->getTwig();
-    }
+		$this->obj = $twig;
+		$this->twig = $twig->getTwig();
+	}
 
-    public function test_anchor()
-    {
-        $actual = $this->twig->render(
-            'anchor',
-            [
-                'uri' => 'news/local/123',
-                'title' => 'My News',
-                'attributes' => ['title' => 'The best news!']
-            ]
-        );
-        $expected = '<a href="http://localhost/index.php/news/local/123" title="The best news!">My News</a>';
-        $this->assertEquals($expected, $actual);
+	public function test_anchor()
+	{
+		$actual = $this->twig->render(
+			'anchor',
+			[
+				'uri' => 'news/local/123',
+				'title' => 'My News',
+				'attributes' => ['title' => 'The best news!']
+			]
+		);
+		$expected = '<a href="http://localhost/index.php/news/local/123" title="The best news!">My News</a>';
+		$this->assertEquals($expected, $actual);
 
-        $actual = $this->twig->render(
-            'anchor',
-            [
-                'uri' => 'news/local/123',
-                'title' => '<s>abc</s>',
-                'attributes' => ['<s>name</s>' => '<s>val</s>']
-            ]
-        );
-        $expected = '<a href="http://localhost/index.php/news/local/123" &lt;s&gt;name&lt;/s&gt;="&lt;s&gt;val&lt;/s&gt;">&lt;s&gt;abc&lt;/s&gt;</a>';
-        $this->assertEquals($expected, $actual);
-    }
+		$actual = $this->twig->render(
+			'anchor',
+			[
+				'uri' => 'news/local/123',
+				'title' => '<s>abc</s>',
+				'attributes' => ['<s>name</s>' => '<s>val</s>']
+			]
+		);
+		$expected = '<a href="http://localhost/index.php/news/local/123" &lt;s&gt;name&lt;/s&gt;="&lt;s&gt;val&lt;/s&gt;">&lt;s&gt;abc&lt;/s&gt;</a>';
+		$this->assertEquals($expected, $actual);
+	}
 
-    public function test_base_url()
-    {
-        $actual = $this->twig->render('base_url');
-        $expected = 'http://localhost/&quot;&gt;&lt;s&gt;abc&lt;/s&gt;&lt;a name=&quot;test';
-        $this->assertEquals($expected, $actual);
-    }
+	public function test_base_url()
+	{
+		$actual = $this->twig->render('base_url');
+		$expected = 'http://localhost/&quot;&gt;&lt;s&gt;abc&lt;/s&gt;&lt;a name=&quot;test';
+		$this->assertEquals($expected, $actual);
+	}
 
-    public function test_site_url()
-    {
-        $actual = $this->twig->render('site_url');
-        $expected = 'http://localhost/index.php/&quot;&gt;&lt;s&gt;abc&lt;/s&gt;&lt;a name=&quot;test';
-        $this->assertEquals($expected, $actual);
-    }
+	public function test_site_url()
+	{
+		$actual = $this->twig->render('site_url');
+		$expected = 'http://localhost/index.php/&quot;&gt;&lt;s&gt;abc&lt;/s&gt;&lt;a name=&quot;test';
+		$this->assertEquals($expected, $actual);
+	}
 }
--- tests/libraries/TwigTest.php
+++ tests/libraries/TwigTest.php
@@ -2,106 +2,106 @@
 
 class TwigTest extends PHPUnit_Framework_TestCase
 {
-    public static function setUpBeforeClass()
-    {
-        parent::setUpBeforeClass();
-        $CI =& get_instance();
-        $CI->load->library('twig');
-        $CI->load->helper('url_helper');
-        $CI->load->helper('form_helper');
-    }
+	public static function setUpBeforeClass()
+	{
+		parent::setUpBeforeClass();
+		$CI =& get_instance();
+		$CI->load->library('twig');
+		$CI->load->helper('url_helper');
+		$CI->load->helper('form_helper');
+	}
 
-    public function testRedner()
-    {
-        $obj = new Twig(['paths' => __DIR__ . '/../templates/']);
-        
-        $data = [
-            'name' => 'CodeIgniter',
-        ];
-        $output = $obj->render('welcome', $data);
-        $this->assertEquals('Hello CodeIgniter!'."\n", $output);
-    }
+	public function testRedner()
+	{
+		$obj = new Twig(['paths' => __DIR__ . '/../templates/']);
 
-    public function testDisplay()
-    {
-        $obj = new Twig(['paths' => __DIR__ . '/../templates/']);
-        
-        $data = [
-            'name' => 'CodeIgniter',
-        ];
-        $obj->display('welcome', $data);
-        $CI =& get_instance();
-        $output = $CI->output->get_output();
-        $this->assertEquals('Hello CodeIgniter!'."\n", $output);
-    }
+		$data = [
+			'name' => 'CodeIgniter',
+		];
+		$output = $obj->render('welcome', $data);
+		$this->assertEquals('Hello CodeIgniter!' . "\n", $output);
+	}
 
-    public function testAddGlobal()
-    {
-        $obj = new Twig(['paths' => __DIR__ . '/../templates/']);
-        $obj->addGlobal('sitename', 'Twig Test Site');
-        
-        $output = $obj->render('global');
-        $this->assertEquals('<title>Twig Test Site</title>'."\n", $output);
-    }
+	public function testDisplay()
+	{
+		$obj = new Twig(['paths' => __DIR__ . '/../templates/']);
 
-    public function testAddFunctionsRunsOnlyOnce()
-    {
-        $obj = new Twig(['paths' => __DIR__ . '/../templates/']);
-        
-        $data = [
-            'name' => 'CodeIgniter',
-        ];
+		$data = [
+			'name' => 'CodeIgniter',
+		];
+		$obj->display('welcome', $data);
+		$CI =& get_instance();
+		$output = $CI->output->get_output();
+		$this->assertEquals('Hello CodeIgniter!' . "\n", $output);
+	}
 
-        $ref_obj = new ReflectionObject($obj);
-        $ref_property = $ref_obj->getProperty('functions_added');
-        $ref_property->setAccessible(true);
-        $functions_added = $ref_property->getValue($obj);
-        $this->assertEquals(false, $functions_added);
+	public function testAddGlobal()
+	{
+		$obj = new Twig(['paths' => __DIR__ . '/../templates/']);
+		$obj->addGlobal('sitename', 'Twig Test Site');
 
-        $output = $obj->render('welcome', $data);
+		$output = $obj->render('global');
+		$this->assertEquals('<title>Twig Test Site</title>' . "\n", $output);
+	}
 
-        $ref_obj = new ReflectionObject($obj);
-        $ref_property = $ref_obj->getProperty('functions_added');
-        $ref_property->setAccessible(true);
-        $functions_added = $ref_property->getValue($obj);
-        $this->assertEquals(true, $functions_added);
+	public function testAddFunctionsRunsOnlyOnce()
+	{
+		$obj = new Twig(['paths' => __DIR__ . '/../templates/']);
 
-        // Calls render() twice
-        $output = $obj->render('welcome', $data);
+		$data = [
+			'name' => 'CodeIgniter',
+		];
 
-        $ref_obj = new ReflectionObject($obj);
-        $ref_property = $ref_obj->getProperty('functions_added');
-        $ref_property->setAccessible(true);
-        $functions_added = $ref_property->getValue($obj);
-        $this->assertEquals(true, $functions_added);
-    }
+		$ref_obj = new ReflectionObject($obj);
+		$ref_property = $ref_obj->getProperty('functions_added');
+		$ref_property->setAccessible(true);
+		$functions_added = $ref_property->getValue($obj);
+		$this->assertEquals(false, $functions_added);
 
-    public function testFunctionAsIs()
-    {
-        $obj = new Twig([
-            'paths' => __DIR__ . '/../templates/',
-            'functions' => ['md5'],
-            'cache' => false,
-        ]);
-        
-        $output = $obj->render('functions_asis');
-        $this->assertEquals('900150983cd24fb0d6963f7d28e17f72'."\n", $output);
-    }
+		$output = $obj->render('welcome', $data);
 
-    public function testFunctionSafe()
-    {
-        $obj = new Twig([
-            'paths' => __DIR__ . '/../templates/',
-            'functions_safe' => ['test_safe'],
-            'cache' => false,
-        ]);
-        
-        $output = $obj->render('functions_safe');
-        $this->assertEquals('<s>test</s>'."\n", $output);
-    }
+		$ref_obj = new ReflectionObject($obj);
+		$ref_property = $ref_obj->getProperty('functions_added');
+		$ref_property->setAccessible(true);
+		$functions_added = $ref_property->getValue($obj);
+		$this->assertEquals(true, $functions_added);
+
+		// Calls render() twice
+		$output = $obj->render('welcome', $data);
+
+		$ref_obj = new ReflectionObject($obj);
+		$ref_property = $ref_obj->getProperty('functions_added');
+		$ref_property->setAccessible(true);
+		$functions_added = $ref_property->getValue($obj);
+		$this->assertEquals(true, $functions_added);
+	}
+
+	public function testFunctionAsIs()
+	{
+		$obj = new Twig([
+			'paths' => __DIR__ . '/../templates/',
+			'functions' => ['md5'],
+			'cache' => false,
+		]);
+
+		$output = $obj->render('functions_asis');
+		$this->assertEquals('900150983cd24fb0d6963f7d28e17f72' . "\n", $output);
+	}
+
+	public function testFunctionSafe()
+	{
+		$obj = new Twig([
+			'paths' => __DIR__ . '/../templates/',
+			'functions_safe' => ['test_safe'],
+			'cache' => false,
+		]);
+
+		$output = $obj->render('functions_safe');
+		$this->assertEquals('<s>test</s>' . "\n", $output);
+	}
 }
 
 function test_safe()
 {
-    return '<s>test</s>';
+	return '<s>test</s>';
 }
